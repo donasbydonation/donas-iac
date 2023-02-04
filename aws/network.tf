5c9f6bd72f8c86740784eeee4aff6a624cc58b56
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-04 16:47:25 +0900

    feat: add private subnet for API layer

--- aws/network.tf
+++ aws/network.tf
@@ -87,3 +87,69 @@ resource "aws_route_table_association" "main_pub_az2" {
   subnet_id      = aws_subnet.main_pub_az2.id
   route_table_id = aws_route_table.main_pub.id
 }
+
+# Private Subnet
+## Route Table
+resource "aws_route_table" "main_priv" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name                      = format("%s-priv", aws_vpc.main.tags.Name)
+    "app.donas.me/tier"       = "production"
+    "arch.donas.me/access"    = "private"
+    "arch.donas.me/layer"     = "api"
+    "obj.donas.me/created-by" = "haeram.kim1"
+    "obj.donas.me/group"      = "network"
+  }
+}
+
+# TODO: EC2-NAT GW
+# resource "aws_route" "main_priv" {
+#   route_table_id         = aws_route_table.main_priv.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway..id
+# }
+
+## API Layer (AZ1)
+resource "aws_subnet" "main_priv_api_az1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = format("%s.2.0/24", var.vpc_cidr_prefix)
+
+  availability_zone = format("%sa", var.app_region)
+
+  tags = {
+    Name                      = format("%s-priv-api-az1", aws_vpc.main.tags.Name)
+    "app.donas.me/tier"       = "production"
+    "arch.donas.me/access"    = "private"
+    "arch.donas.me/layer"     = "api"
+    "obj.donas.me/created-by" = "haeram.kim1"
+    "obj.donas.me/group"      = "network"
+  }
+}
+
+resource "aws_route_table_association" "main_priv_api_az1" {
+  subnet_id      = aws_subnet.main_priv_api_az1.id
+  route_table_id = aws_route_table.main_priv.id
+}
+
+## API Layer (AZ2)
+resource "aws_subnet" "main_priv_api_az2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = format("%s.5.0/24", var.vpc_cidr_prefix)
+
+  availability_zone = format("%sc", var.app_region)
+
+  tags = {
+    Name                      = format("%s-priv-api-az2", aws_vpc.main.tags.Name)
+    "app.donas.me/tier"       = "production"
+    "arch.donas.me/access"    = "private"
+    "arch.donas.me/layer"     = "api"
+    "obj.donas.me/created-by" = "haeram.kim1"
+    "obj.donas.me/group"      = "network"
+  }
+}
+
+resource "aws_route_table_association" "main_priv_api_az2" {
+  subnet_id      = aws_subnet.main_priv_api_az2.id
+  route_table_id = aws_route_table.main_priv.id
+}
