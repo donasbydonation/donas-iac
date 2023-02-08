9b3559e4fc68de7ee62d58da55b5b919b06b3370
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-09 00:21:45 +0900

    feat: add CodeDeploy IAM policy arn datasource

--- aws/data.tf
+++ aws/data.tf
@@ -1,4 +1,5 @@
-# - Ubuntu 20.04 AMI
+# - AMI
+## - Ubuntu 20.04
 data "aws_ami" "ubuntu_2004" {
   most_recent = true
   owners      = [var.ami_owner]
@@ -12,4 +13,14 @@ data "aws_ami" "ubuntu_2004" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+}
+
+# - Policy
+## - CodeDeploy
+data "aws_iam_policy" "aws_codedeploy_role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+data "aws_iam_policy" "aws_codedeploy_fullaccess" {
+  arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
 }
