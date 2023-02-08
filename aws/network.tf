303f1ee7e1379fcbc90b011374b463713dc517e0
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-08 21:26:33 +0900

    fix: modify network tags

--- aws/network.tf
+++ aws/network.tf
@@ -18,18 +18,18 @@ module "vpc" {
   }
 
   public_subnet_tags = {
-    "arch.donas.me/access"    = "public"
-    "arch.donas.me/layer"     = "public"
+    "arch.donas.me/access" = "public"
+    "arch.donas.me/layer"  = "public"
   }
 
   private_subnet_tags = {
-    "arch.donas.me/access"    = "private"
-    "arch.donas.me/layer"     = "api"
+    "arch.donas.me/access" = "private"
+    "arch.donas.me/layer"  = "server"
   }
 
   database_subnet_tags = {
-    "arch.donas.me/access"    = "private"
-    "arch.donas.me/layer"     = "db"
+    "arch.donas.me/access" = "private"
+    "arch.donas.me/layer"  = "db"
   }
 }
 
@@ -38,7 +38,7 @@ module "vpc" {
 # -------------------------
 # https://registry.terraform.io/modules/int128/nat-instance/aws/2.1.0
 module "nat" {
-  source = "int128/nat-instance/aws"
+  source  = "int128/nat-instance/aws"
   version = "2.1.0"
 
   name                        = var.app_name
@@ -51,6 +51,8 @@ module "nat" {
     "app.donas.me/tier"       = "production"
     "obj.donas.me/created-by" = "haeram.kim1"
     "obj.donas.me/group"      = "network"
+    "arch.donas.me/access"    = "public"
+    "arch.donas.me/layer"     = "public"
   }
 }
 
@@ -58,7 +60,7 @@ resource "aws_eip" "nat" {
   network_interface = module.nat.eni_id
 
   tags = {
-    "Name" = format("nat-instance-%s", var.app_name)
+    "Name"                    = format("nat-instance-%s", var.app_name)
     "app.donas.me/tier"       = "production"
     "obj.donas.me/created-by" = "haeram.kim1"
     "obj.donas.me/group"      = "network"
