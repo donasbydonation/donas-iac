c96f5748ca7fe3daa599c2702bae931a0fb55c34
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-08 21:14:35 +0900

    feat: SG for server

--- aws/ec2-api.tf
+++ aws/ec2-api.tf
@@ -1,7 +1,7 @@
 # - Ubuntu 20.04 AMI
 data "aws_ami" "ubuntu_2004" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners      = [var.ami_owner]
 
   filter {
     name   = "name"
@@ -18,4 +18,61 @@ data "aws_ami" "ubuntu_2004" {
 resource "aws_key_pair" "ssh" {
   key_name   = format("%s-ssh", var.app_name)
   public_key = file("./data/ec2-ssh.pub")
+}
+
+# - SG
+resource "aws_security_group" "server" {
+  name        = format("%s-server", var.app_name)
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = format("%s-server", var.app_name)
+    "app.donas.me/tier"       = "production"
+    "obj.donas.me/created-by" = "haeram.kim1"
+    "obj.donas.me/group"      = "network"
+    "arch.donas.me/access"    = "private"
+    "arch.donas.me/layer"     = "api"
+  }
+}
+
+## - SG ingress rule: API/TCP
+resource "aws_security_group_rule" "server_ing_api" {
+  type                     = "ingress"
+  from_port                = var.api_port
+  to_port                  = var.api_port
+  protocol                 = "TCP"
+  source_security_group_id = aws_security_group.lb.id
+  security_group_id        = aws_security_group.server.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## - SG ingress rule: Web/TCP
+resource "aws_security_group_rule" "server_ing_web" {
+  type                     = "ingress"
+  from_port                = var.web_port
+  to_port                  = var.web_port
+  protocol                 = "TCP"
+  source_security_group_id = aws_security_group.lb.id
+  security_group_id        = aws_security_group.server.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+## - SG egress rule: */*
+resource "aws_security_group_rule" "server_eg_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.server.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
