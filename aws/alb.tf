2eadd9ca630635eeeabdb18ef02527f2a243b8e9
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-09 22:21:26 +0900

    feat: add AWS ALB

--- aws/alb.tf
+++ aws/alb.tf
@@ -1,41 +1,88 @@
-# - SG
-resource "aws_security_group" "lb" {
-  name   = format("%s-lb", var.app_name)
-  vpc_id = module.vpc.vpc_id
+# - ALB
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = format("%s-lb", var.app_name)
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+
+  security_group_rules = {
+    ingress_all_http = {
+      type        = "ingress"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress_all_https = {
+      type        = "ingress"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress_web = {
+      type        = "ingress"
+      from_port   = var.web_port
+      to_port     = var.web_port
+      protocol    = "TCP"
+      source_security_group_id = aws_security_group.server.id
+    }
+    egress_all_https = {
+      type        = "egress"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress_web = {
+      type        = "egress"
+      from_port   = var.web_port
+      to_port     = var.web_port
+      protocol    = "TCP"
+      source_security_group_id = aws_security_group.server.id
+    }
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "asg"
+      backend_protocol = "HTTP"
+      backend_port     = var.web_port
+      target_type      = "instance"
+    }
+  ]
+
+  https_listeners = [
+    {
+      port                 = 443
+      protocol             = "HTTPS"
+      certificate_arn      = module.acm.acm_certificate_arn
+      target_group_index   = 0
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "redirect"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  ]
 
   tags = {
-    Name                   = format("%s-lb", var.app_name)
     "app.donas.me/tier"    = "production"
-    "obj.donas.me/group"   = "access-control"
+    "obj.donas.me/group"   = "lb"
     "arch.donas.me/access" = "public"
     "arch.donas.me/layer"  = "public"
-  }
-}
-
-## - SG ingress rule: */*
-resource "aws_security_group_rule" "lb_ing_all" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-## - SG egress rule: */*
-resource "aws_security_group_rule" "lb_eg_all" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lb.id
-
-  lifecycle {
-    create_before_destroy = true
   }
 }
