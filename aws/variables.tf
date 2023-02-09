3713d6d403d476bf1bb643e9c0060d51506dd992
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-10 00:09:25 +0900

    feat: add AWS RDS credentials

--- aws/variables.tf
+++ aws/variables.tf
@@ -33,3 +33,19 @@ variable "AWS_ASG_USER_DATA" {
 variable "route53_hz" {
   type = string
 }
+
+variable "AWS_RDS_DBNAME" {
+  type = string
+}
+
+variable "AWS_RDS_PORT" {
+  type = string
+}
+
+variable "AWS_RDS_USERNAME" {
+  type = string
+}
+
+variable "AWS_RDS_PASSWORD" {
+  type = string
+}
