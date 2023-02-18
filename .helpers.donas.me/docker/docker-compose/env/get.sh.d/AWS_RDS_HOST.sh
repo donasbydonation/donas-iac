1e542c4b1cf49f305e07f5aae28239ceb7fab3f7
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-19 00:27:39 +0900

    fix: export rds name w/ tf output

--- .helpers.donas.me/docker/docker-compose/env/get.sh.d/AWS_RDS_HOST.sh
+++ .helpers.donas.me/docker/docker-compose/env/get.sh.d/AWS_RDS_HOST.sh
@@ -5,6 +5,5 @@ set -e
 ROOT_DIR=${ROOT_DIR?"ERROR: Required environment variable not set"}
 TF_DIR="$ROOT_DIR/terraform"
 
-terraform -chdir=$TF_DIR state show $(terraform -chdir=$TF_DIR state list | grep aws_db_instance) \
-    | grep address \
-    | sed -E 's/.+"(.+)"/\1/g'
+terraform -chdir=$TF_DIR output AWS_RDS_HOST \
+    | sed -E 's/"(.+)"/\1/g'
