930b80164f2c76ead50098f9dfe5f523f8a483bb
haeramkeem <ewqdsacxz2345@gmail.com>
2023-02-18 23:43:47 +0900

    refactor: pre-push hook

--- git-hooks/pre-push.sh
+++ git-hooks/pre-push.sh
@@ -2,53 +2,31 @@
 
 set -e
 
-source ./config.env
-source ./config.cred.env
+export ROOT_DIR=$(pwd)
+HOOK_NAME=$(basename $0)
 
-# -------------------------
-# GitHub settings
-# -------------------------
-echo "[github/repository-secrets] Sync github repository secrets..."
-./helpers/github/secret/gsync.sh
-source ./autogen.gh-secret.env
-echo
+# - Github
+## - Secrets
+echo "[$HOOK_NAME/github/sync-secrets] Sync GitHub action secrets..."
+$ROOT_DIR/.helpers.donas.me/github/workflows/secret/export.sh
 
-echo "[github/repository-secrets] Used AWS ASG user_data is:"
-base64 -d <<< "$AWS_ASG_USER_DATA"
-echo
+# - Sourcing generated env
+source $ROOT_DIR/.env.config
+source $ROOT_DIR/.env.credentials
+source $ROOT_DIR/.env.autogen.docker-compose
+source $ROOT_DIR/.env.autogen.tf-var
+source $ROOT_DIR/.env.autogen.gh-secrets
 
-# -------------------------
-# Docker settings
-# -------------------------
-echo "[docker/docker-compose] Generating 'docker-compose.b64' file..."
-./helpers/docker/make-b64.sh
-echo
+# - Terraform
+## - plan
+echo "[$HOOK_NAME/terraform/plan] Planning TF codes..."
+terraform -chdir=terraform plan
 
-echo "[docker/docker-compose] Committing changes if exists..."
-if `git ls-files -m --exclude-standard | grep docker/docker-compose.b64 &> /dev/null`; then
-    git add docker/docker-compose.b64
-    git commit -m "fix: docker-compose.b64 (autocommit; reason unknown)"
-fi
-echo
-
-echo "[docker/docker-compose] Used docker-compose.yaml is:"
-base64 -d docker/docker-compose.b64
-echo
-
-# -------------------------
-# Terraform settings
-# -------------------------
-cd aws
-echo "[terraform/plan] Planning terraform infrastructure..."
-source ./shim.env
-terraform plan
-echo
-
-echo "[terraform/fmt] Formatting terraform codes..."
-terraform fmt -diff
-cd ..
-if `git ls-files -m --exclude-standard | grep aws/ &> /dev/null`; then
-    git add aws/
-    git commit -m "chore: fmt"
-fi
-echo
+## - fmt
+echo "[$HOOK_NAME/terraform/fmt] Formatting TF codes..."
+for tf in $(terraform -chdir=terraform fmt); do
+    if `git ls-files -m --exclude-standard | grep terraform/$tf &> /dev/null`; then
+        git add terraform/$tf
+        git commit -m "fmt: $tf"
+    fi
+done
