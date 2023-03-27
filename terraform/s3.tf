817474dd9328886a3dba3ad7f2eccfbc5ce59bd9
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-27 21:58:55 +0900

    feat: public admin web console bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -9,3 +9,37 @@ module "s3_bucket" {
     enabled = true
   }
 }
+
+module "s3_admin_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.7.0"
+
+  bucket = format("%s-admin-bucket", local.app.name)
+
+  attach_policy = true
+  policy  = data.aws_iam_policy_document.admin_web_public_read.json
+
+  website = {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+data "aws_iam_policy_document" "admin_web_public_read" {
+  statement {
+    sid = "PublicReadGetObject"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      format("arn:aws:s3:::%s-admin-bucket/*", local.app.name),
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
