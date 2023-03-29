c8553d49cbc736fe1ba13c1f84398aef4cc353c0
haeramkeem <ewqdsacxz2345@gmail.com>
2023-03-29 23:28:00 +0900

    revert: cdn distribution for admin console

--- terraform/admin.tf
+++
@@ -1,7 +0,0 @@
-module "admin" {
-  source = "./admin"
-
-  hz_name = local.route53.hz_name
-  hz_id = module.route53_hz.route53_zone_zone_id[local.route53.hz_name]
-  record_name = local.route53.admin_record_name
-}
