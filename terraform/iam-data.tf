bb47e92d214bcfb81303a90f847f210d56da8673
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-23 17:09:39 +0900

    fix: edit group policy to grant full access to subpath for the bucket

--- terraform/iam-data.tf
+++ terraform/iam-data.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "s3_public_read_bucket_full_access" {
     ]
 
     resources = [
-      module.s3_public_read_bucket.s3_bucket_arn,
+      format("%s/*", module.s3_public_read_bucket.s3_bucket_arn),
     ]
   }
 }
