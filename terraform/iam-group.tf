f372a687c6af2a951b86f4f7aa97af0022ec09c3
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-23 16:32:46 +0900

    feat: public read bucket iam group

--- terraform/iam-group.tf
+++ terraform/iam-group.tf
@@ -23,3 +23,17 @@ resource "aws_iam_group_policy_attachment" "gh_action_robot" {
   group      = aws_iam_group.gh_action_robot.name
   policy_arn = data.aws_iam_policy.aws_administrator_access.arn
 }
+
+#
+# S3://donas-public-read-bucket robot account group
+#
+resource "aws_iam_group" "s3_public_read_bucket_robot" {
+  name = format("%s-public-read-bucket-robot", local.app.name)
+  path = "/"
+}
+
+resource "aws_iam_group_policy" "s3_public_read_bucket_robot" {
+  name  = "s3-public-read-bucket-robot"
+  group = aws_iam_group.s3_public_read_bucket_robot.name
+  policy = data.aws_iam_policy_document.s3_public_read_bucket_full_access.json
+}
