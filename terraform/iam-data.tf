c987efe68aee82c375c3f3457e3fd91174b57367
haeramkeem <ewqdsacxz2345@gmail.com>
2023-04-23 16:01:10 +0900

    feat: import IAM group policy

---
+++ terraform/iam-data.tf
@@ -0,0 +1,3 @@
+data "aws_iam_policy" "aws_administrator_access" {
+  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
--- terraform/iam-group.tf
+++ terraform/iam-group.tf
@@ -1,9 +1,25 @@
+#
+# Admin account group
+#
 resource "aws_iam_group" "admin" {
   name = "admin"
   path = "/"
 }
 
+resource "aws_iam_group_policy_attachment" "admin" {
+  group      = aws_iam_group.admin.name
+  policy_arn = data.aws_iam_policy.aws_administrator_access.arn
+}
+
+#
+# GitHub action robot account group
+#
 resource "aws_iam_group" "gh_action_robot" {
   name = "gh-action-robot"
   path = "/"
+}
+
+resource "aws_iam_group_policy_attachment" "gh_action_robot" {
+  group      = aws_iam_group.gh_action_robot.name
+  policy_arn = data.aws_iam_policy.aws_administrator_access.arn
 }
